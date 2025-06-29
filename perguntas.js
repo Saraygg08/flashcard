381ab401f726f908d907c42bdeaae11b799f90fa
Saraygg08 <saray.guerrero@escola.pr.gov.br>
2025-06-28 22:10:04 -0300

    Update and rename scrip.html to perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Química',
+    'O que é matéria?',
+    'É tudo aquilo que tem massa e ocupa lugar no espaço.'
+)
+
+criaCartao(
+    'Química',
+    'A massa, o peso e o volume são propriedades da matéria?',
+    'Gerais.'
+)
+
+criaCartao(
+    'Química',
+    'A temperatura na qual uma sustância sólida passa ao estado líquido denomina-se?',
+    'Ponto de fusão.'
+)
