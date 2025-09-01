f9d6253c98b0de7ccc5fbcc78cdf7b653d16eab7
Saraygg08 <saray.guerrero@escola.pr.gov.br>
2025-09-01 11:29:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 function criaCartao(categoria, pergunta, resposta) {
     console.log(categoria, pergunta, resposta)
 }
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+
+    criaCartao.classNome='cartao'
+
+    criaCartao.innerHTML=`
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    </div>
+    `
+    container
+
+}
